748d95e87cad1b5faa0a0cf94c4fe3cbce461170
MY Han <han.my75@gmail.com>
2018-09-27 14:36:02 +0000

    Remove unused script

--- configs/etc/profile.d/autostart.sh
+++ configs/etc/profile.d/autostart.sh
@@ -1,13 +1,11 @@
 # launch our autostart apps (if we are on the correct tty)
-RUN_FOLDER=/home/pi/home_assistant
+RUN_FOLDER=/home/pi
 
 if [ "`tty`" = "/dev/tty1" ]; then
     # Turn off LED and HDMI
-    /home/pi/turn_off.sh
+    $RUN_FOLDER/turn_off.sh
 
     # Launch flic daemon
-    sudo $RUN_FOLDER/flicd -d -f /home/pi/flic.sqlite3
+    sudo $RUN_FOLDER/flicd -d -f $RUN_FOLDER/flic.sqlite3
 
-    # Run MJPEG Stream Server
-    mjpg_streamer -o "output_http.so" -i "input_raspicam.so -x 640 -y 480 -fps 15" &
 fi
