06e62319d15a3f46c5adda43c3b853c82f526335
MY Han <han.my75@gmail.com>
2020-10-29 11:30:35 +0000

    Update script

--- configs/home/pi/trans_radio.sh
+++ configs/home/pi/trans_radio.sh
@@ -1,11 +1,30 @@
 #! /bin/bash
 
 # Config for MBC
-export MBC_URL="$(./get_radio_url.py 'MBC FM')"
-envsubst  < nginx.conf.template > /etc/nginx/nginx.conf
-systemctl restart nginx
+config_mbc()
+{
+	export MBC_URL=$1
+	envsubst  < nginx.conf.template > /etc/nginx/nginx.conf
+	systemctl restart nginx
+}
 
 # Config for KBS
-killall ffmpeg
-KBS_URL="$(./get_radio_url.py 'KBS 1FM')"
-ffmpeg -re  -i ${KBS_URL} -c:a copy -bsf:a aac_adtstoasc -f flv rtmp://localhost/live/kbs &>/dev/null &
+config_kbs()
+{
+	killall ffmpeg
+	KBS_URL=$1
+	ffmpeg -re  -i ${KBS_URL} -c:a copy -bsf:a aac_adtstoasc -f flv rtmp://localhost/live/kbs &>/dev/null &
+}
+
+
+# Main
+case "$1" in
+	start)
+		config_mbc "$(./get_radio_url.py 'MBC FM')"
+		config_kbs "$(./get_radio_url.py 'KBS 1FM')"
+	;;
+	stop)
+		config_mbc "rtmp://localhost"
+		config_kbs ""
+	;;
+esac
