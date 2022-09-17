f7182fc1e0c3c78f145aead56990b5ab2d5e1313
MY Han <han.my75@gmail.com>
2022-09-17 14:37:01 +0100

    Update HA config

--- configs/home/pi/trans_radio.sh
+++ configs/home/pi/trans_radio.sh
@@ -1,5 +1,24 @@
 #! /bin/bash
 
+# DASH folder
+DASH_FOLDER=/var/www/html/dash
+
+# Radio Channel Map
+declare -A channelMap
+channelMap["mbc"]="MBC FM4U"
+channelMap["sbs"]="SBS Power FM"
+channelMap["kbs"]="KBS 2FM"
+
+# Check File Exist
+isFileExist()
+{
+    result=0
+    if [ -f "$DASH_FOLDER/$1" ]; then
+        result=1
+    fi
+    echo $result
+}
+
 # Config ffmpge
 config_ffmpeg()
 {
@@ -8,14 +27,27 @@ config_ffmpeg()
 	ffmpeg -re  -i ${URL} -c:a copy -bsf:a aac_adtstoasc -f flv rtmp://localhost/live/${INDEX} &>/dev/null &
 }
 
+# Check and Start transcoding
+#runTranscoding()
+#{
+#    mpdFile=$1.mpd
+#
+#}
+
 # Main
 case "$1" in
 	start)
-		killall ffmpeg
-		config_ffmpeg "$(/home/pi/get_radio_url.py 'MBC FM4U')" "mbc"
-		config_ffmpeg "$(/home/pi/get_radio_url.py 'SBS Power FM')" "sbs"
-		config_ffmpeg "$(/home/pi/get_radio_url.py 'KBS 2FM')" "kbs"
+        for _key in "${!channelMap[@]}"
+        do
+            # Check MPD file
+            if [ "$(isFileExist "$_key.mpd")" -ne 1 ]; then
+                echo "Start $_key transcoding"
+                url="$(/home/pi/get_radio_url.py "${channelMap[$_key]}")"
+                config_ffmpeg "$url" "$_key"
+            fi
+        done
 	;;
+
 	stop)
 		killall ffmpeg
 		rm -rf /var/www/html/dash/*
