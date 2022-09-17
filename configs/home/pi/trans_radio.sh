91dc3b5b64ba7091e438fc73aabff7aa84655c53
MY Han <han.my75@gmail.com>
2022-09-17 14:59:54 +0100

    Update HA config

--- configs/home/pi/trans_radio.sh
+++ configs/home/pi/trans_radio.sh
@@ -9,14 +9,10 @@ channelMap["mbc"]="MBC FM4U"
 channelMap["sbs"]="SBS Power FM"
 channelMap["kbs"]="KBS 2FM"
 
-# Check File Exist
-isFileExist()
+# Check Transcoding is running
+isTranscodeRunning()
 {
-    result=0
-    if [ -f "$DASH_FOLDER/$1" ]; then
-        result=1
-    fi
-    echo $result
+    echo "$(ps -ef | grep ffmpeg | grep "$1" | wc -l)"
 }
 
 # Config ffmpge
@@ -27,20 +23,13 @@ config_ffmpeg()
 	ffmpeg -re  -i ${URL} -c:a copy -bsf:a aac_adtstoasc -f flv rtmp://localhost/live/${INDEX} &>/dev/null &
 }
 
-# Check and Start transcoding
-#runTranscoding()
-#{
-#    mpdFile=$1.mpd
-#
-#}
-
 # Main
 case "$1" in
 	start)
         for _key in "${!channelMap[@]}"
         do
             # Check MPD file
-            if [ "$(isFileExist "$_key.mpd")" -ne 1 ]; then
+            if [ "$(isTranscodeRunning "$_key")" -ne "1" ]; then
                 echo "Start $_key transcoding"
                 url="$(/home/pi/get_radio_url.py "${channelMap[$_key]}")"
                 config_ffmpeg "$url" "$_key"
